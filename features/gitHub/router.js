68d28890cdc0515b0c5d0fbabae7902e66fe6b0f
CJ Hatch <siege.hatch@gmail.com>
2021-10-16 13:36:13 -0500

    Get user and repo name from url

--- features/gitHub/router.js
+++ features/gitHub/router.js
@@ -3,7 +3,14 @@ const gitHubRouter = new express.Router()
 
 gitHubRouter.get('/', async (req, res) => {
     const repoUrl = req.query.url
-    res.send(`${req.query.url} sure looks like a great repo!`)
+    const splitUrl = repoUrl.split('/')
+    const match = (/\/([A-Za-z0-9-]+)\/([A-Za-z0-9_.-]+)/).exec(repoUrl)
+    const user = match[1]
+    const repo = match[2]
+    console.log(repoUrl)
+    console.log(match)
+    res.send(`User: ${user}
+Repository: ${repo}`)
 })
 
 module.exports = gitHubRouter
