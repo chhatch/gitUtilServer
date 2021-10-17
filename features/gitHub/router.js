91ca5751844b00e23176fbffe69c8a208c313905
CJ Hatch <siege.hatch@gmail.com>
2021-10-17 09:17:36 -0500

    Filter pulls and send json data

--- features/gitHub/router.js
+++ features/gitHub/router.js
@@ -6,15 +6,23 @@ gitHubRouter.get('/', async (req, res) => {
     const gh = new GitHub()
     const repoUrl = req.query.url
     const splitUrl = repoUrl.split('/')
-    const match = (/\/([A-Za-z0-9-]+)\/([A-Za-z0-9_.-]+)/).exec(repoUrl)
+    const match = /\/([A-Za-z0-9-]+)\/([A-Za-z0-9_.-]+)/.exec(repoUrl)
     const user = match[1]
     const repoName = match[2]
 
     const repo = gh.getRepo(user, repoName)
-    const queryRes = await repo.listPullRequests({state: 'open'})
-    const openPulls = queryRes.data
+    const openPulls = []
+    let page = 1
+    while (true) {
+        const queryRes = await repo.listPullRequests({ state: 'open', page, per_page: 100 })
+        if (queryRes.data.length) {
+            openPulls.push(...queryRes.data)
+            page++
+        } else break
+    }
     
-    res.send(`${openPulls.length} open pull requests`)
+
+    res.json(openPulls.map(pull => ({title: pull.title, url: pull.url})))
 })
 
 module.exports = gitHubRouter
