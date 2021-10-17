1c167cfbf454640e6319fb7274ed1cb778cf1e06
CJ Hatch <siege.hatch@gmail.com>
2021-10-17 09:01:00 -0500

    Get open pull request using GitHub api

--- features/gitHub/router.js
+++ features/gitHub/router.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const gitHubRouter = new express.Router()
+const GitHub = require('github-api')
 
 gitHubRouter.get('/', async (req, res) => {
+    const gh = new GitHub()
     const repoUrl = req.query.url
     const splitUrl = repoUrl.split('/')
     const match = (/\/([A-Za-z0-9-]+)\/([A-Za-z0-9_.-]+)/).exec(repoUrl)
     const user = match[1]
-    const repo = match[2]
-    console.log(repoUrl)
-    console.log(match)
-    res.send(`User: ${user}
-Repository: ${repo}`)
+    const repoName = match[2]
+
+    const repo = gh.getRepo(user, repoName)
+    const queryRes = await repo.listPullRequests({state: 'open'})
+    const openPulls = queryRes.data
+    
+    res.send(`${openPulls.length} open pull requests`)
 })
 
 module.exports = gitHubRouter
