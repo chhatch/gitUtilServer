9b653e49df2caf2dfb0ad135f5d2e77d3c4e9bee
CJ Hatch <siege.hatch@gmail.com>
2021-10-18 13:51:19 -0500

    Handle error extract user and repo name

--- features/gitHub/services/userRepoFromUrl.js
+++ features/gitHub/services/userRepoFromUrl.js
@@ -2,7 +2,17 @@ const userRepoFromUrl = (repoUrl) => {
     const match = /\/([A-Za-z0-9-]+)\/([A-Za-z0-9_.-]+)/.exec(repoUrl)
     const user = match[1]
     const repoName = match[2]
-    return {user, repoName}
+    if (!user) {
+        let e = new Error('Invalid GitHub username')
+        e.status = 400
+        throw e
+    }
+    if (!repoName) {
+        let e = new Error('Invalid GitHub repository name')
+        e.status = 400
+        throw e
+    }
+    return { user, repoName }
 }
 
 module.exports = userRepoFromUrl
