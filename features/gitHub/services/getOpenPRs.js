4d2b33a0f2664986117799213dc516f0709b133b
CJ Hatch <siege.hatch@gmail.com>
2021-10-18 14:17:12 -0500

    Handle errors fetching pull requests

--- features/gitHub/services/getOpenPRs.js
+++ features/gitHub/services/getOpenPRs.js
@@ -2,22 +2,30 @@ const sleep = (ms) =>
     new Promise((resolve, _) => setTimeout(() => resolve(), ms))
 
 const getOpenPrs = async (ghRepo) => {
-    let openPulls = []
-    let page = 1
-    while (true) {
-        const queryRes = await ghRepo.listPullRequests({
-            state: 'open',
-            page,
-            per_page: 100,
-        })
-        if (queryRes.data.length) {
-            openPulls.push(...queryRes.data)
-            page++
-            await sleep(20)
-        } else break
+    try {
+        let openPulls = []
+        let page = 1
+        while (true) {
+            const queryRes = await ghRepo.listPullRequests({
+                state: 'open',
+                page,
+                per_page: 100,
+            })
+            if (queryRes.data.length) {
+                openPulls.push(...queryRes.data)
+                page++
+                await sleep(20)
+            } else break
+        }
+        return openPulls
+    } catch (e) {
+        //this handles private repos too
+        if (e.response && e.response.status === 404) {
+            let e = new Error('Repository not found')
+            e.status = 404
+            throw e
+        } else throw e
     }
-
-    return openPulls
 }
 
 module.exports = getOpenPrs
