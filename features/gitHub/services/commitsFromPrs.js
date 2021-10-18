61bf8b4a2b0d6a5d75f673b892e3f53c5ff841e9
CJ Hatch <siege.hatch@gmail.com>
2021-10-18 14:22:08 -0500

    Error message for fetching commits for pr

--- features/gitHub/services/commitsFromPrs.js
+++ features/gitHub/services/commitsFromPrs.js
@@ -2,15 +2,19 @@ const sleep = (ms) =>
     new Promise((resolve, _) => setTimeout(() => resolve(), ms))
 
 const commitsFromPRs = async (pulls) => {
-    for (let i = 0, len = openPulls.length; i < len; i++) {
-        const pull = openPulls[i]
-        const commits = (
-            await axios.get(pull.commits_url, {
-                headers: { Authorization: `token ${gitHubApiKey}` },
-            })
-        ).data
-        pull.numberOfCommits = commits.length
-        sleep(20)
+    try {
+        for (let i = 0, len = openPulls.length; i < len; i++) {
+            const pull = openPulls[i]
+            const commits = (
+                await axios.get(pull.commits_url, {
+                    headers: { Authorization: `token ${gitHubApiKey}` },
+                })
+            ).data
+            pull.numberOfCommits = commits.length
+            sleep(20)
+        }
+        return openPulls
+    } catch (e) {
+        throw new Error('Error getting commits')
     }
-    return openPulls
 }
