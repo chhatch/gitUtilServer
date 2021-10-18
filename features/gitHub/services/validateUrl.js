f5efd71103044c618288d4f5d7e635a5ce5e5852
CJ Hatch <siege.hatch@gmail.com>
2021-10-18 14:29:06 -0500

    Forgot to export. How was the router working before?

--- features/gitHub/services/validateUrl.js
+++ features/gitHub/services/validateUrl.js
@@ -1,10 +1,15 @@
 const validUrl = require('valid-url')
 
-const validateUrl = url => {
-const valid = validUrl.isUri(url)
+const validateUrl = (url) => {
+    const valid = validUrl.isUri(url)
+    console.log(url, valid)
     if (valid) {
-return url
+        return url
     } else {
-    const e =  new Error('Invalid URL')}
-    e.status = 400
+        const e = new Error('Invalid URL')
+        e.status = 400
+        throw e
+    }
 }
+
+module.exports = validateUrl
